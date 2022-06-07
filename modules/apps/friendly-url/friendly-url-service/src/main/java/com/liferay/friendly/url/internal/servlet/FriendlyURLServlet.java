15f4f3119ea096b2837f23a90c1fd4fceeb624d1
Jose L. Bango <jose.bango@liferay.com>
2022-06-07 13:31:57 +0200

    LPS-154862 Use new property to check if company locales are available in site administration

--- modules/apps/friendly-url/friendly-url-service/src/main/java/com/liferay/friendly/url/internal/servlet/FriendlyURLServlet.java
+++ modules/apps/friendly-url/friendly-url-service/src/main/java/com/liferay/friendly/url/internal/servlet/FriendlyURLServlet.java
@@ -59,10 +59,12 @@ import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.util.PortalInstances;
+import com.liferay.portal.util.PrefsPropsUtil;
 import com.liferay.portal.util.PropsValues;
 import com.liferay.portlet.AsyncPortletServletRequest;
 import com.liferay.redirect.model.RedirectEntry;
@@ -279,10 +281,17 @@ public class FriendlyURLServlet extends HttpServlet {
 
 				boolean localeUnavailable = false;
 
+				boolean allowCompanyLocalesInSiteAdmin =
+					PrefsPropsUtil.getBoolean(
+						PortalUtil.getCompanyId(httpServletRequest),
+						PropsKeys.LOCALE_ALLOW_COMPANY_LOCALES_IN_SITE_ADMIN,
+						PropsValues.LOCALE_ALLOW_COMPANY_IN_SITE_ADMIN);
+
 				if (Validator.isNotNull(i18nLanguageId) &&
 					!LanguageUtil.isAvailableLocale(
 						group.getGroupId(), i18nLanguageId) &&
-					(!PortalUtil.isControlPanelPath(path) ||
+					(!allowCompanyLocalesInSiteAdmin ||
+					 !PortalUtil.isControlPanelPath(path) ||
 					 !LanguageUtil.isAvailableLocale(i18nLanguageId))) {
 
 					localeUnavailable = true;
